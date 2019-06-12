950b607765e801c5ab887505c1fde053c0f5cdad
Sesh <51412574+musesha@users.noreply.github.com>
2019-06-12 11:29:11 +0200

    Question 2

--- variables.js
+++ variables.js
@@ -1,1 +1,18 @@
-// you solution should be here
+// Question1
+var birthYear = 1992;
+var futureYear = 2030;
+var currentAge1 = futureYear - birthYear;
+console.log(currentAge1);
+
+var currentAge2 = 2019 - birthYear;
+console.log(currentAge2);
+
+// Question2
+var currentAge = 27, maxAge = 100, estAmount = 4300;
+var ageDiff = maxAge - currentAge; 
+
+var amountEaten = estAmount/ageDiff;
+
+console.log("You will need to eat " 
++ amountEaten + " for " + ageDiff + 
+" per year to survive til the age of " + maxAge);
